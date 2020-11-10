3d6304c9a045c9982d09c7701adfd6bbf395ddda
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2020-11-11 00:45:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -60,6 +60,33 @@ class App extends Component {
                     </div>
                 </section>
                 
+
+
+
+
+const App = () => {
+  const [contacts, setContacts] = React.useState([
+    { "id": 1,"photo": Avatar, "name": "Tamma Everill", "phone": "+351 888 411 5474" },
+    { "id": 2,"photo": Avatar, "name": "Alejandrina Alexis", "phone": "+62 188 649 7200" },
+    { "id": 3,"photo": Avatar, "name": "Hakim Bruntjen", "phone": "+86 241 773 8545" },
+    { "id": 4,"photo": Avatar, "name": "Prudi Dagwell", "phone": "+62 606 216 1097" },
+    { "id": 5,"photo": Avatar, "name": "Prent Frizell", "phone": "+86 808 891 5427" },
+    { "id": 6,"photo": Avatar, "name": "Curtis Enterlein", "phone": "+64 836 110 1773" },
+    { "id": 7,"photo": Avatar, "name": "Tamma Everill", "phone": "+351 888 411 5474" },
+      ]);
+
+{contacts.map(({ id, photo, name, phone }) => (
+          <div className="contact-container" key={id}>
+            <img src={photo} alt={name} className="avatar" />
+            <div className="text-container">
+              <h5 className="name">{name}</h5>
+              <p className="phone">{phone}</p>
+            </div>
+          </div>
+        ))}
+
+
+
                 <section className='section'>
                     <div className='container'>
                         <div>
